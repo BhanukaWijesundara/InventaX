2f24c72b8c81741b859321fd7c811ef1b7b70684
BhanukaWijesundara <bhanukahw04@gmail.com>
2025-05-16 17:22:31 +0530

    Update UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,3 +3,24 @@ package service;
 import model.User;
 import util.FileHandler;
 import java.util.List;
+
+public class UserService {
+
+    public void addUser(User user) {
+        FileHandler.writeUser(user);
+    }
+
+    public List<User> getAllUsers() {
+        return FileHandler.readUsers();
+    }
+
+    public void updateUser(List<User> users) {
+        FileHandler.rewriteUsers(users);
+    }
+
+    public void deleteUser(String userId) {
+        List<User> users = FileHandler.readUsers();
+        users.removeIf(u -> u.getUserId().equals(userId));
+        FileHandler.rewriteUsers(users);
+    }
+}
