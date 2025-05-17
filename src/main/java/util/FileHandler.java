aea41f1d3804681a8a7be423c85170eae439c783
Hasanthii <hasanthichamoda3@gmail.com>
2025-05-17 12:34:08 +0530

    Add file handler method to write purchase order details to purchases.txt

--- src/main/java/util/FileHandler.java
+++ src/main/java/util/FileHandler.java
@@ -1,4 +1,23 @@
 package util;
 
+import model.PurchaseOrder;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileHandler {
+    public static void writePurchase(PurchaseOrder order){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Hasanthi\\Documents\\GitHub\\InventaX\\data\\purchases.txt",true))){
+            writer.write(order.getPurchaseId() + "," + order.getItemId() + "," + order.getQuantity() + "," +
+                    order.getDate() + "," + order.getSupplierId() + "," + order.getStatus());
+            writer.newLine();
+        }
+        catch (IOException e) {
+            System.err.println("Error writing purchase to file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
+
