aeb0f58884d54c67ed0149269b5bab123784be69
BhanukaWijesundara <bhanukahw04@gmail.com>
2025-05-17 14:20:39 +0530

    Update UserServlet.java

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -3,4 +3,10 @@ package servlet;
 import model.User;
 import service.UserService;
 
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
