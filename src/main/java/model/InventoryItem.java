ce2b69f192b7d13695ba40e119d38082b1bc2cd6
Achintha-L <achinthalakshan275@gmail.com>
2025-05-19 12:06:55 +0530

    Update InventoryItem.java

--- src/main/java/model/InventoryItem.java
+++ src/main/java/model/InventoryItem.java
@@ -1,4 +1,61 @@
 package model;
 
 public class InventoryItem {
+        private String itemId;
+        private String itemName;
+        private int quantity;
+        private String expiryDate;
+        private String category;
+
+        // Constructor
+        public InventoryItem(String itemId, String itemName, int quantity, String expiryDate, String category) {
+            this.itemId = itemId;
+            this.itemName = itemName;
+            this.quantity = quantity;
+            this.expiryDate = expiryDate;
+            this.category = category;
+        }
+
+        // Getters
+        public String getItemId() {
+            return itemId;
+        }
+
+        public String getItemName() {
+            return itemName;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public String getExpiryDate() {
+            return expiryDate;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        // Setters
+        public void setItemId(String itemId) {
+            this.itemId = itemId;
+        }
+
+        public void setItemName(String itemName) {
+            this.itemName = itemName;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public void setExpiryDate(String expiryDate) {
+            this.expiryDate = expiryDate;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+    }
 }
