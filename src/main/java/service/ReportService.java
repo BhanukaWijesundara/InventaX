5a4760ba69af505e8052b1ecd00672a12852de12
Shehan14 <kavindashehan1018@gmail.com>
2025-05-21 01:39:54 +0530

    Update ReportService.java

--- src/main/java/service/ReportService.java
+++ src/main/java/service/ReportService.java
@@ -15,6 +15,18 @@ import java.util.List;
 import java.util.UUID;
 
 public class ReportService {
+    private static final String BASE_DIR = System.getProperty("user.dir");
+    private static final String DATA_DIR = BASE_DIR + File.separator + "data";
 
+    public ReportService() {
+        try {
+            File dataDir = new File(DATA_DIR);
+            if (!dataDir.exists()) {
+                dataDir.mkdirs();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
 }
