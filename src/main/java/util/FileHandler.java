158bf9e9b5b91ffe3fc49cb92de4d66bd8dbe9ac
Shehan14 <kavindashehan1018@gmail.com>
2025-05-21 18:52:30 +0530

    Update FileHandler.java

--- src/main/java/util/FileHandler.java
+++ src/main/java/util/FileHandler.java
@@ -112,5 +112,17 @@ public class FileHandler {
         return list;
     }
 
+    //Report
+    public static void writeReport(List<InventoryItem> sortedItems) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Shehan\\Documents\\Modules\\SE1020 -OOP\\Project\\final\\projectXX\\data\\report.txt"))) {
+            for (InventoryItem item : sortedItems) {
+                writer.write(item.getItemId() + "," + item.getItemName() + "," +
+                        item.getQuantity() + "," + item.getExpiryDate() + "," + item.getCategory());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
